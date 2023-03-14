c524bc99b4357876106cf2f5515c7e7ae2f1f6f4
Olivier3456 <olivier.larrey@gmail.com>
2023-03-14 08:14:31 +0100

    Suppr hat UI

--- Assets/Scripts/HatUI.cs
+++ Assets/Scripts/HatUI.cs
@@ -7,21 +7,41 @@ public class HatUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
     
+    
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.enabled = false;
+        }
+    }
+
 
     public void GrabAHat()
     {
-        text.text = "Grab a hat.";
+        text.text = "text.text = Grab a hat.";
+        Debug.Log("Grab a hat.");
     }
 
     public void PutTheHatOnYourHead()
     {
         text.text = "Put the hat on your head.";
+        Debug.Log("text.text = Put the hat on your head.");
     }
 
-    public void NoText()
-    {
-        text.text = "";
-    }
+    
 
-   
+
+
+
+
 }
