a69ec3bcb544653c4f64a91936e486c295b5c739
Olivier3456 <olivier.larrey@gmail.com>
2023-03-14 22:20:51 +0100

    Trail position change OK

--- Assets/Scripts/SaveYourCreation.cs
+++ Assets/Scripts/SaveYourCreation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
 using UnityEngine.UIElements;
 
 public class SaveYourCreation : MonoBehaviour
@@ -8,13 +9,14 @@ public class SaveYourCreation : MonoBehaviour
 
     [SerializeField] private Transform placeToSave;
 
-    [SerializeField] private int MAX_POSITIONS = 100;
+    [SerializeField] private GameObject trailPrefab;
+
+    
 
     public void SaveCreation()
     {
         GameObject[] trails = GameObject.FindGameObjectsWithTag("Trail");
-        GameObject parentTrailSave = Instantiate(new GameObject());
-
+       
 
         Vector3 offset = placeToSave.position - transform.position;
 
@@ -22,21 +24,32 @@ public class SaveYourCreation : MonoBehaviour
 
         for (int i = 0; i < trails.Length; i++)
         {
-            trails[i].GetComponent<TrailRenderer>().emitting = false;
-            trails[i].tag = null;
+            TrailRenderer renderer = trails[i].GetComponent<TrailRenderer>();
 
-            Vector3[] TrailRecorded = new Vector3[MAX_POSITIONS];
-            int numberOfPositions = trails[i].GetComponent<TrailRenderer>().GetPositions(TrailRecorded);
+            Vector3[] TrailRecorded = new Vector3[renderer.positionCount];
+            int numberOfPositions = renderer.GetPositions(TrailRecorded);
 
             for (int j = 0; j < TrailRecorded.Length; j++)
-            {
-                
-                TrailRecorded[j] = TrailRecorded[j] + offset;
-                
+            {                
+                TrailRecorded[j] = TrailRecorded[j] + offset;                
             }
-            trails[i].GetComponent<TrailRenderer>().SetPositions(TrailRecorded);
+
+            GameObject newTrail = Instantiate(trailPrefab, TrailRecorded[0], trails[i].transform.rotation);
+            newTrail.tag = "Finish";
+
+            TrailRenderer actualTrailRendeder = trails[i].GetComponent<TrailRenderer>();
+            TrailRenderer newTrailRenderer = newTrail.GetComponent<TrailRenderer>();            
+            newTrailRenderer.widthMultiplier = actualTrailRendeder.widthMultiplier;
+            newTrailRenderer.startColor = actualTrailRendeder.startColor;
+            newTrailRenderer.endColor = actualTrailRendeder.endColor;
 
 
+            newTrailRenderer.AddPositions(TrailRecorded);
+          
+
+            trails[i].SetActive(false);
+
+            
             //trails[i].transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
             //trails[i].transform.SetParent(parentTrailSave.transform);
         }
