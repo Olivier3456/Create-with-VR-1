e15be6fec6ef1dd4d65ebd93f1b5d10dd1156e4e
Olivier3456 <olivier.larrey@gmail.com>
2023-03-15 08:51:18 +0100

    Challenge 2 - step 8 OK but problem of material color

--- Assets/Scripts/SaveYourCreation.cs
+++ Assets/Scripts/SaveYourCreation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Xsl;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.RenderGraphModule;
 using UnityEngine.UIElements;
@@ -11,47 +12,76 @@ public class SaveYourCreation : MonoBehaviour
 
     [SerializeField] private GameObject trailPrefab;
 
-    
+    [SerializeField] private float reductionFactor = 3;
+
 
     public void SaveCreation()
     {
         GameObject[] trails = GameObject.FindGameObjectsWithTag("Trail");
-       
+        Mesh trailMesh = new Mesh();
 
-        Vector3 offset = placeToSave.position - transform.position;
 
-        
+        //Vector3 offset = placeToSave.position - transform.position;
+
 
         for (int i = 0; i < trails.Length; i++)
         {
             TrailRenderer renderer = trails[i].GetComponent<TrailRenderer>();
+            renderer.BakeMesh(trailMesh, true);
+            //renderer.BakeMesh(trailMesh, Camera.main, true);
 
+            GameObject trailObject = new GameObject("Trail Save");
+
+
+            #region Calcule la position moyenne des vertices de la trail pour l'appliquer en offset à toutes les positions des vertices et les relocaliser.
             Vector3[] TrailRecorded = new Vector3[renderer.positionCount];
             int numberOfPositions = renderer.GetPositions(TrailRecorded);
+            Vector3 averagePosition = Vector3.zero;
 
             for (int j = 0; j < TrailRecorded.Length; j++)
-            {                
-                TrailRecorded[j] = TrailRecorded[j] + offset;                
-            }
+            { averagePosition += TrailRecorded[j]; }
+            averagePosition /= TrailRecorded.Length;
 
-            GameObject newTrail = Instantiate(trailPrefab, TrailRecorded[0], trails[i].transform.rotation);
-            newTrail.tag = "Finish";
-
-            TrailRenderer actualTrailRendeder = trails[i].GetComponent<TrailRenderer>();
-            TrailRenderer newTrailRenderer = newTrail.GetComponent<TrailRenderer>();            
-            newTrailRenderer.widthMultiplier = actualTrailRendeder.widthMultiplier;
-            newTrailRenderer.startColor = actualTrailRendeder.startColor;
-            newTrailRenderer.endColor = actualTrailRendeder.endColor;
+            for (int j = 0; j < TrailRecorded.Length; j++)
+            { TrailRecorded[j] += averagePosition; }
+            #endregion
 
 
-            newTrailRenderer.AddPositions(TrailRecorded);
-          
+            MeshFilter meshFilter = trailObject.AddComponent<MeshFilter>();
+            MeshRenderer meshRenderer = trailObject.AddComponent<MeshRenderer>();
+            meshFilter.sharedMesh = trailMesh;
+            trailObject.transform.position = placeToSave.position;
+            trailObject.transform.localScale = trailObject.transform.localScale / reductionFactor;
 
-            trails[i].SetActive(false);
+            Material material = new Material(Shader.Find("Universal Render Pipeline/Unlit"));
+            material.color = renderer.startColor;
+            meshRenderer.material = material;
 
-            
-            //trails[i].transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            //trails[i].transform.SetParent(parentTrailSave.transform);
+
+
+            #region Méthode de déplacement de la trail en créant une nouvelle trail pour y mettre les coordonnées des vertices de l'ancienne, avec un offset (fonctionne).
+            //TrailRenderer renderer = trails[i].GetComponent<TrailRenderer>();
+            //Vector3[] TrailRecorded = new Vector3[renderer.positionCount];
+            //int numberOfPositions = renderer.GetPositions(TrailRecorded);
+
+            //for (int j = 0; j < TrailRecorded.Length; j++)
+            //{                
+            //    TrailRecorded[j] = TrailRecorded[j] + offset;                
+            //}
+
+            //GameObject newTrail = Instantiate(trailPrefab, TrailRecorded[0], trails[i].transform.rotation);
+            //newTrail.tag = "Finish";
+
+            //TrailRenderer actualTrailRenderer = trails[i].GetComponent<TrailRenderer>();
+            //TrailRenderer newTrailRenderer = newTrail.GetComponent<TrailRenderer>();            
+            //newTrailRenderer.widthMultiplier = renderer.widthMultiplier / reductionFactor;
+            //newTrailRenderer.startColor = renderer.startColor;
+            //newTrailRenderer.endColor = renderer.endColor;
+
+            //newTrailRenderer.AddPositions(TrailRecorded);
+
+            //trails[i].SetActive(false);
+            #endregion
         }
     }
 }
