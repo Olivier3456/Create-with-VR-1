428a665482a6b76f60987a14d95f1beccdcb26c6
Olivier3456 <olivier.larrey@gmail.com>
2023-03-19 14:42:14 +0100

    Fade on teleportation OK

--- Assets/Scripts/BallBounce.cs
+++ Assets/Scripts/BallBounce.cs
@@ -13,8 +13,8 @@ public class BallBounce : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         float volume = Mathf.Clamp(rigid.velocity.magnitude * 0.4f, 0, 1);
-        
-        Debug.Log("rigid.velocity.magnitude of the ball's rigidbody: " + rigid.velocity.magnitude);
+
+        //    Debug.Log("rigid.velocity.magnitude of the ball's rigidbody: " + rigid.velocity.magnitude);
         audioSource.volume = volume;
         audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
     }
